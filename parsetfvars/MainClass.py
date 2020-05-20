837ad97ace89ce308a9a32869441fa1b56ec12dc
kilwa <kilwacero@gmail.com>
2020-05-20 15:17:23 +0200

    mp multiline done but need testing

--- parsetfvars/MainClass.py
+++ parsetfvars/MainClass.py
@@ -16,6 +16,16 @@ class ParseTfvars:
         return [line for line in open(self.file).readlines() if var
                 in line]
 
+    def blockRead(self, var):
+        with open(self.file) as input_data:
+            for line in input_data:
+                if line.startswith(var):
+                    break
+            for line in input_data:
+                if line.strip() == r'}':
+                    break
+                yield line
+
     def st(self, var):
         st = self.simpleline(var)
         return str(st[0].split('=')[1].split('"')[1])
@@ -33,10 +43,21 @@ class ParseTfvars:
         return list(re.sub('"', '', re.sub(r'(^\[|\]$)', '', re.sub(r'\s', '',
                     lt[0]).replace("%s=" % var, ""))).split(","))
 
+    # def mp(self, var):
+    #     mp = self.simpleline(var)
+    #     var = json.loads(re.sub(r'(,)(?!.*\1)', '', re.sub(r'\]', '],',
+    #                      re.sub(' ', '', re.sub(r'\=', ':', re.sub(r'\=',
+    #                             ':', mp[0], count=1
+    #                             ).split(':')[1]), count=1))))
+    #     return var
+
     def mp(self, var):
-        mp = self.simpleline(var)
-        var = json.loads(re.sub(r'(,)(?!.*\1)', '', re.sub(r'\]', '],',
-                         re.sub(' ', '', re.sub(r'\=', ':', re.sub(r'\=',
-                                ':', mp[0], count=1
-                                ).split(':')[1]), count=1))))
-        return var
+        dicc = {}
+        for line in self.blockRead(var):
+            dicc[re.sub(r'  \"|\" $', '',
+                        line.split(
+                         '=')[0])] =\
+                          re.sub(r' \[|]', '', re.sub(r'\"|\n', '',
+                                 line.split('=')[1]))
+        jjson = json.loads(str(dicc).replace("'", '"'))
+        return jjson
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,11 @@
+rolo = {"fruta"   = ["sandia", "melon"] "animal"  = ["perro", "gato"] "colores" = ["negro", "blanco"]}
+
+mapa2 = {
+  "fruta" = ["nara", "melon"]
+  "animal" = ["perro", "gato"]
+  "colores" = ["negro", "blanco"]
+}
 cadena = "cadena mas menos larga"
 numero = 12
 logico = true
 lista  = ["string", "string2", "string3"]
-mapa = {"fruta"   = ["sandia", "melon"] "animal"  = ["perro", "gato"] "colores" = ["negro", "blanco"]}
